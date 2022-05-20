15557ce001ecb69da5c637671f12f1f400766c23
NeverMind109 <vladgladkih123@gmail.com>
2022-05-20 19:07:17 +0300

    Created: Added Product Modal

--- src/js/_components.js
+++ src/js/_components.js
@@ -2,3 +2,4 @@ import "./components/range-slider";
 import "./components/quiz";
 import "./components/accordion";
 import "./components/map";
+import "./components/product";
--- src/js/_vendor.js
+++ src/js/_vendor.js
@@ -1,2 +1,3 @@
 import "./vendor/focus-visible.js";
 import "./vendor/nouislider.min.js";
+import "./vendor/clamp.min.js";
